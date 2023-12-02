96db8fcd7f8a68edbec0b4f9b97d8456be7874f2
Cal <callumburnsoregan@gmail.com>
2023-12-02 00:37:18 +0000

    dc

--- main/app/src/App.tsx
+++ main/app/src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import './App.css';
-import { HeyImCal } from './components/HeyImCal';
+import React from "react";
+import "./App.css";
+import { HeyImCal } from "./components/HeyImCal";
+import { NekoPonent } from "./components/Neko";
 
 function App() {
-  return (
-    <div className="App">
-      <HeyImCal name="Cal"/>
-        <header className="App-header">  
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>
+                <HeyImCal name="Cal" />
+                <NekoPonent />
+                <header className="App-header"></header>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- main/app/src/pages/index.tsx
+++ main/app/src/pages/index.tsx
@@ -1,11 +1,12 @@
-import {Container, Heading, Text} from "@chakra-ui/react";
+import { Container, Heading, Text } from "@chakra-ui/react";
 
 export default function indexPage() {
     return (
         <Container>
             <Heading>
                 <Text>Hello World!</Text>
+                <script src="./oneko/oneko.js"></script>
             </Heading>
         </Container>
-    )
+    );
 }
