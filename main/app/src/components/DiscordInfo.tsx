43310ddf09236eb9cd28d1c5fa968d7ebf7ea627
Cal <callumburnsoregan@gmail.com>
2023-12-05 16:01:52 +0000

    Added colors to the status value

--- main/app/src/components/DiscordInfo.tsx
+++ main/app/src/components/DiscordInfo.tsx
@@ -5,25 +5,6 @@ const url = "https://api.lanyard.rest/v1/users/242276511028084738";
 let avatar =
     "https://cdn.discordapp.com/avatars/242276511028084738/66b26bbc03c85fa6659b6ae21c8ff485.png?size=4096";
 export const DiscordInfo: React.FC = () => {
-    const [status, setStatus] = useState<string | null>(null);
-
-    useEffect(() => {
-        const getDiscordInfomation = async () => {
-            try {
-                const response = await fetch(url, {
-                    method: "GET",
-                });
-                const data = await response.json();
-                const discordStatus = data.data.discord_status;
-                setStatus(discordStatus);
-            } catch (error) {
-                console.error("Error fetching Discord information", error);
-            }
-        };
-
-        getDiscordInfomation();
-    }, []);
-
     return (
         <div className="center-items main2">
             <Box
@@ -42,15 +23,50 @@ export const DiscordInfo: React.FC = () => {
                     borderWidth="3px"
                     borderColor="Grey"
                 ></Image>
-                <Box marginLeft={5}
-                fontSize={16}>
+                <Box
+                    marginLeft={5}
+                    fontSize={16}
+                    fontWeight="500"
+                    fontStyle="italic"
+                >
                     <Text display="flex">
                         <Text>He/Him</Text>
                     </Text>
                     <Text>Tag: caldev</Text>
-                    <Text>Status: {status}</Text>
+                    <Text>
+                        Status: <GetColor />
+                    </Text>
                 </Box>
             </Box>
         </div>
+    );
+};
+
+export const GetColor: React.FC = () => {
+    const [status, setStatus] = useState<string | null>(null);
+
+    useEffect(() => {
+        const getDiscordInfomation = async () => {
+            try {
+                const response = await fetch(url, {
+                    method: "GET",
+                });
+                const data = await response.json();
+                const discordStatus = data.data.discord_status;
+                setStatus(discordStatus);
+            } catch (error) {
+                console.error("Error fetching Discord information", error);
+            }
+        };
+
+        getDiscordInfomation();
+    }, []);
+    return (
+        <Text
+            display="inline-block"
+            color={status === "online" ? "#8EFF9B" : "#FF6565"}
+        >
+            {status}
+        </Text>
     );
 };
